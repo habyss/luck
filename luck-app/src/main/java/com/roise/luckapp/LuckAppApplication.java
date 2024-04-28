935ad790101a82e18012fd811dc152bb5483efec
hankun <Kun.Han@guwave.com>
2024-04-28 20:15:54 +0800

    init

---
+++ luck-app/src/main/java/com/roise/luckapp/LuckAppApplication.java
@@ -0,0 +1,13 @@
+package com.roise.luckapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckAppApplication.class, args);
+    }
+
+}
---
+++ luck-mysql/src/main/java/com/roise/luckmysql/LuckMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.roise.luckmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckMysqlApplication.class, args);
+    }
+
+}
